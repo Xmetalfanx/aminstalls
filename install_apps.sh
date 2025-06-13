2ff70e0815e3202563bdde118f133dd4e02a1908
Xmetalfanx <Xmetalfanx@yahoo.com>
2025-06-13 00:43:59 -0400

    refactored script

--- install_apps.sh
+++ install_apps.sh
@@ -1,27 +1,85 @@
 #!/bin/bash
 
+browsers="floorp librewolf"
+common_apps="bitwarden freetube materialious deadbeef czkawka keepassxc stacer ente-auth"
+extras="responsively vscodium"
+unofficial_apps="brave strawberry"
+total_extras="etcher figma-linux gimp ungoogled-chromium ocenaudio libreoffice vcelestia firedragon warp-terminal mediaelch spotify-dl photogimp tabbyb tenacity zen-browser ente-auth"
+
 function installApp {
     install="am -ia --user"
-
-    $install "$@"
+    for app in "$@"; do
+        echo "Installing $app..."
+        if command -v "$app" &>/dev/null; then
+            echo "$app is already installed."
+        else
+            $install "$app"
+            if [ $? -eq 0 ]; then
+                echo "$app installed successfully."
+            else
+                echo "Failed to install $app."
+            fi
+        fi
+    done
 
     sleep 2
 }
 
-echo "Installing Browsers"
-installApp "floorp librewolf"
+function installMenu() {
+    clear
+    x=1
+    while [ $x -eq 1 ]; do
+        echo "==========================="
+        echo "AM - App Installer Menu"
+        echo "==========================="
+        echo "Select apps to install:"
 
-echo "Installing some commonly used apps"
-installApp bitwarden freetube materialious deadbeef czkawka keepassxc stacer ente-auth
+        echo "1) Browsers: $browsers"
+        echo "2) Commonly used apps: $common_apps"
+        echo "3) Extras: $extras"
+        echo "4) Unofficial apps: $unofficial_apps"
+        echo "5) Total extras: $total_extras"
+        echo -e "\nQ) Quit"
+        echo -n "Please select an option (1-6): "
+        read -r choice
 
+        case $choice in
+        1)
+            echo "Installing Browsers: $browsers"
+            installApp $browsers
+            ;;
 
-# Idea: on some slim installs (like my 16GB NVME Chromebook) these are "not needed"
-## hence why these are commented out "by default"
-# echo "Installing Extras"
-# installApp responsively vscodium
+        2)
+            echo "Installing Commonly used apps: $common_apps"
+            installApp $common_apps
+            ;;
 
-# echo "Installing Unofficial apps"
-# installApp brave strawberry
+        3)
+            echo "Installing Extras: $extras"
+            installApp $extras
+            ;;
 
-# echo "Installing total extras"
-# installApp etcher figma-linux gimp ungoogled-chromium ocenaudio libreofficevcelestia firedragon warp-terminal mediaelch spotify-dl photogimp tabbyb tenacity  zen-browser ente-auth
+        4)
+            echo "Installing Unofficial apps: $unofficial_apps"
+            installApp $unofficial_apps
+            ;;
+
+        5)
+            echo "Installing Total Extras: $total_extras"
+            installApp $total_extras
+            ;;
+        [Qq])
+            echo "Exiting menu."
+            x=0
+            ;;
+
+        *)
+            echo "Invalid option. Try again."
+            sleep 2
+            ;;
+        esac
+    done
+}
+
+# Call the menu function
+installMenu
