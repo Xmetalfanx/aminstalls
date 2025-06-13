d471e56abd64bd13d29b2e40e212502b066ec155
Xmetalfanx <Xmetalfanx@yahoo.com>
2025-06-13 15:45:19 -0400

    Refactored install_am.sh
    remember that i commented out the wget line here

--- install_am.sh
+++ install_am.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+amconfigdir=/home/$USER/.config/appman/
+
 echo "Downloading am"
-wget -q https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER
+#wget -q https://raw.githubusercontent.com/ivan-hc/AM/main/AM-INSTALLER
+sleep 2
 
 echo "Setting script to be executable"
 chmod a+x ./AM-INSTALLER
 
 echo "Running installer"
+sleep 2
 ./AM-INSTALLER
+
+echo "Creating am config location"
+mkdir $amconfigdir
+
 
 echo "Opening Config file - type desired location of installed apps"
 micro ~/.config/appman/appman-config
