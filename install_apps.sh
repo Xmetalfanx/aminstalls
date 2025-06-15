4610656537b8acf6a4bcfc10e3324f8aa0b07144
Xmetalfanx <Xmetalfanx@yahoo.com>
2025-06-15 15:11:15 -0400

    Added Graphics Apps catagory, splitting out some of the multimedia apps
    - also added Xnviewmp

--- install_apps.sh
+++ install_apps.sh
@@ -2,15 +2,15 @@
 
 function assign_app_variables() {
 
+    # Unoffical apps are in the unofficial catagory no matter what type of app they are 
     browsers="floorp librewolf"
     developer_apps="figma-linux gitkraken responsively vscodium"
-    # this does not include "unofficial" marked browsers like brave (in am unofficial)
     extra_browsers="brave firedragon ungoogled-chromium zen-browser"
-    # this also does not include "unofficial" marked apps like strawberry (in am unofficial)
-    multimedia_apps="deadbeef gimp mediaelch ocenaudio photogimp tenacity xnviewmp"
+    graphics_apps="gimp photogimp xnviewmp"
+    multimedia_apps="deadbeef mediaelch ocenaudio tenacity"
     security_apps="bitwarden ente-auth keepassxc"
     system_apps="czkawka etcher stacer"
-    terminal_apps="tabbyb warp-terminal"
+    terminal_apps="tabby warp-terminal"
     youtube_apps="freetube materialious"
     total_extras="celestia libreoffice nheko spotify-dl"
     unofficial_apps="strawberry"
@@ -84,16 +84,21 @@ function installMenu() {
 
         echo -e "1) ${bold}Browsers:${end_bold} $browsers"
         echo -e "2) ${bold}Extra Browsers:${end_bold} $extra_browsers"
-        echo -e "3) ${bold}Developer apps:${end_bold} $developer_apps"
-        echo -e "4) ${bold}Multimedia apps:${end_bold} $multimedia_apps"
-        echo -e "5) ${bold}Security apps:${end_bold} $security_apps"
-        echo -e "6) ${bold}System apps:${end_bold} $system_apps"
-        echo -e "7) ${bold}Terminal apps:${end_bold} $terminal_apps"
-        echo -e "8) ${bold}YouTube apps:${end_bold} $youtube_apps"
-        echo -e "9) ${bold}Total extras:${end_bold} $total_extras"
-        echo -e "10) ${bold}Unofficial apps:${end_bold} $unofficial_apps"
+        echo -e "3) ${bold}Graphics apps:${end_bold} $graphics_apps"
+        echo -e "4) ${bold}Developer apps:${end_bold} $developer_apps"
+        echo -e "5) ${bold}Multimedia apps:${end_bold} $multimedia_apps"
+        echo -e "6) ${bold}Security apps:${end_bold} $security_apps"
+        echo -e "7) ${bold}System apps:${end_bold} $system_apps"
+        echo -e "8) ${bold}Terminal apps:${end_bold} $terminal_apps"
+        echo -e "9) ${bold}YouTube apps:${end_bold} $youtube_apps"
+        echo -e "10) ${bold}Total extras:${end_bold} $total_extras"
+        echo -e "11) ${bold}Unofficial apps:${end_bold} $unofficial_apps"
+        echo -e "----------------------------------"
+        echo -e "X) Xmetal seletion - a custom selection of apps that i use (using this is the point of why i made the script)"
+        echo -e "S) Xmetal slim - a custom selection of apps that i use, but slimmed down"
+        echo -e "----------------------------------"
         echo -e "\nQ) Quit"
-        echo -n "Please select an option (1-10): "
+        echo -n "Please select an option (1-11): "
         read -r choice
 
         case $choice in
@@ -108,41 +113,46 @@ function installMenu() {
             ;;
 
         3)
+            echo "Installing Graphics apps: $graphics_apps"
+            installApp $graphics_apps
+            ;;
+
+        4)
             echo "Installing Developer apps: $developer_apps"
             installApp $developer_apps
             ;;
 
-        4)
+        5)
             echo "Installing Multimedia apps: $multimedia_apps"
             installApp $multimedia_apps
             ;;
 
-        5)
+        6)
             echo "Installing Security apps: $security_apps"
             installApp $security_apps
             ;;
 
-        6)
+        7)
             echo "Installing System apps: $system_apps"
             installApp $system_apps
             ;;
 
-        7)
+        8)
             echo "Installing Terminal apps: $terminal_apps"
             installApp $terminal_apps
             ;;
 
-        8)
+        9)
             echo "Installing YouTube apps: $youtube_apps"
             installApp $youtube_apps
             ;;
 
-        9)
+        10)
             echo "Installing Total extras: $total_extras"
             installApp $total_extras
             ;;
 
-        10)
+        11)
             echo "Installing Unofficial apps: $unofficial_apps"
             installApp $unofficial_apps
             ;;
